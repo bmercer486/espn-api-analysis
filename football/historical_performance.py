e5706ea1a0aa6d6c2ab5992d893217887ab3b5db
bmercer486 <bmercer486@gmail.com>
2020-11-11 13:46:26 +0100

    Update to use swid and espn_s2 cookies instead of username/password

--- football/historical_performance.py
+++ football/historical_performance.py
@@ -1,7 +1,9 @@
 from espn_api.football import League
 
 # Init
-league = League(league_id=235837, year=2019, username='mercinator486', password='Throwaway123$')
+#league = League(league_id=235837, year=2019, username='mercinator486', password='Throwaway123$')
+league = League(league_id=235837, year=2019, swid='960A4466-0190-46A6-926B-4C7411602D3B',
+espn_s2='AEC1%2BEdz7P6rOeLgGrgN163zuJFd65XBRcdxIoBDZ62cOYs0fTwu9XmlSl6tpkVyAMdB27LeKUJKiyMwpjfW%2B%2BxwCXMvN3qa8GWKDyMq0WxgC5EZy1TSU3Ws6DVbW2GSYr7kZwIKjL%2BKER4VhxC%2BUQ7RAH2SVtfSWn2RxibenHT%2FagC1ijS%2BAgz4YQ47QeS3adaNl7WB%2FFUh9nAliyVf8TYScLPkhiaxOUkAZ3tVjsxtAMFATxHv3Ylpjz%2BU5yuUBqn5jR2%2FDM%2FaPN%2BCe9Zb0FLu')
 settings = league.settings
 
 # Print teams to test
