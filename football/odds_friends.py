d51757699b183625320dda0fb0eb192e3dab8fea
bmercer486 <bmercer486@gmail.com>
2022-07-19 14:39:41 +0200

    Playoff odds for friends and Johnson league are cleaned up in terms of - output files are formatted better and include year, week and nsim - work with updated leagueInfo files that contain static league info

--- football/odds_friends.py
+++ football/odds_friends.py
@@ -7,24 +7,32 @@ import time
 import csv
 import sys
 
-# Leage data
-id = 235837
+# Import static league data: id, swid, and espn_s2
+from leagueInfo_friends import *
+
+# Year and rules
 year = 2021
-swid='960A4466-0190-46A6-926B-4C7411602D3B'
-espn_s2='AEC1%2BEdz7P6rOeLgGrgN163zuJFd65XBRcdxIoBDZ62cOYs0fTwu9XmlSl6tpkVyAMdB27LeKUJKiyMwpjfW%2B%2BxwCXMvN3qa8GWKDyMq0WxgC5EZy1TSU3Ws6DVbW2GSYr7kZwIKjL%2BKER4VhxC%2BUQ7RAH2SVtfSWn2RxibenHT%2FagC1ijS%2BAgz4YQ47QeS3adaNl7WB%2FFUh9nAliyVf8TYScLPkhiaxOUkAZ3tVjsxtAMFATxHv3Ylpjz%2BU5yuUBqn5jR2%2FDM%2FaPN%2BCe9Zb0FLu'
-# For saving simulation results'
+nseeds = 6
+extraWL = True
+week_end = 14 # final week to consider for calculating odds
+
+# League name for saving files
 leaguename = 'friends'
+
+# Simulation settings
+seed = 2021 # Arbitrary
+rng = np.random.default_rng(seed)
+# Distribution is 'normal', 'lognormal', or 'random'
+distribution = 'random'
+nsim = 100
 
 # Define the league
 league = League(league_id=id, year=year, swid=swid, espn_s2=espn_s2)
 
-# What is the current week?
+# What is the current week? (not yet played)
 # league.current_week gives the current week real-time
 week_current = league.current_week
 week_current = 14
-
-# Last week of the regular season
-week_end = 14 # final week to consider for calculating odds
 
 # Don't do calculation if it is requested for week at or beyond end of regular season
 if(week_current > week_end):
@@ -33,22 +41,11 @@ if(week_current > week_end):
 
 # List of teams
 teams = league.teams
-
-# Teams, seeds, extra WL settings
 nteams = len(teams)
-nseeds = 6
-extraWL = True
-
-# Simulation settings
-seed = 2021 # Arbitrary
-rng = np.random.default_rng(seed)
-# Distribution is 'normal', 'lognormal', or 'random'
-distribution = 'random'
-nsim = 100000
 
 # The 'outcomes' array stores how many times each team got a particular seed
 # The row is the team and the column is the outcome
-# e.g. outcomes[3,1] will be how often team #3 in the 'teams' list
+# e.g. outcomes[3,1] will be how often team #4 in the 'teams' list
 # got 2nd place in the final standings
 outcomes = np.zeros( (nteams,nteams), dtype=int)
 start_time = time.time()
@@ -114,11 +111,11 @@ df = pd.DataFrame({"Team name": [t.team_name for t in teams],
 # print(df)
 
 # Save it for later
-filename = leaguename + "_playoff_odds" + "_heading_into_week" + str(week_current) + ".pkl"
+filename = leaguename + "_playoff_odds_heading_into_week" + str(week_current) + "_year" + str(year) + "_nsim" + str(nsim) + ".pkl"
 df.to_pickle(filename)
 
 # Write results to csv file
-filename = "playoff_odds_" + leaguename + "_heading_into_week_" + str(week_current) + "_year_" + str(year) + ".csv"
+filename = leaguename + "_playoff_odds_heading_into_week" + str(week_current) + "_year" + str(year) + "_nsim" + str(nsim) + ".csv"
 with open(filename, mode='w', newline='') as csvfile:
     fwriter = csv.writer(csvfile)
     fwriter.writerow(['Team name','% chance make playoffs','% chance 1 seed','% chance 2 seed','% chance 3 seed','% chance 4 seed','% chance 5 seed','% chance 6 seed'])
--- football/odds_johnsonLeague.py
+++ football/odds_johnsonLeague.py
@@ -2,26 +2,37 @@ from espn_api.football import League
 from standingsFunctions import *
 from playoffOddsFunctions import *
 import numpy as np
+import pandas as pd
 import time
 import csv
 import sys
 
-id = 906364
+# Import static league data: id, swid, and espn_s2
+from leagueInfo_Johnson import *
+
+# Year and rules
 year = 2021
-swid = '960A4466-0190-46A6-926B-4C7411602D3B'
-espn_s2 =  'AEBZY7c0EB8kSiEbA3guFdDr3H7bmSnSPEmxEXiaEeEz4kfMGA6%2BDxk3U3eznlcz9lquh0Tw86tX220Bcz%2FCBU8ynKt57Z6ERGrGwY8XpwvcSwIf2o8IfSEL7C%2BVH8FNKhKLDa34sSGsEUReuTSrTSrYWqax1l0hSgcDADebrE%2FWcSgAo6GMXOd%2Fp9vF8pYcehYt4fmlVJx4NBysKrboEPXdith2XzWvOVSWdyKe7PVWSp7TtFjkmritmcyacT4Tdxi8gt2Kn5QVbiQqE5wVoVJf'
-# For saving simulation results
+nseeds = 6
+extraWL = True
+week_end = 14 # final week to consider for calculating odds
+
+# League name for savings files
 leaguename = 'Johnson\'s League'
+
+# Simulation settings
+seed = 2021 # Arbitrary
+rng = np.random.default_rng(seed)
+# Distribution is 'normal', 'lognormal', or 'random'
+distribution = 'random'
+nsim = 100
 
 # Define the league
 league = League(league_id=id, year=year, swid=swid, espn_s2=espn_s2)
 
-# What is the current week?
+# What is the current week? (not yet played)
 # league.current gives the current week real-time
 week_current = league.current_week
-
-# Last week of the regular season
-week_end = 14 # final week to consider for calculating odds
+week_current = 14
 
 # Don't do calculation if it is requested for week at or beyond end of regular season
 if(week_current > week_end):
@@ -30,21 +41,11 @@ if(week_current > week_end):
 
 # List of teams
 teams = league.teams
-
-# Teams, seeds, extra WL settings
 nteams = len(teams)
-nseeds = 6
-extraWL = True
-
-# Simulation settings
-seed = 2021 # Arbitrary
-rng = np.random.default_rng(seed)
-distribution = 'normal'
-nsim = 100000
 
 # The 'outcomes' array stores how many times each team got a particular seed
 # The row is the team and the column is the outcome
-# e.g. outcomes[3,1] will be how often team #3 in the 'teams' list
+# e.g. outcomes[3,1] will be how often team #4 in the 'teams' list
 # got 2nd place in the final standings
 outcomes = np.zeros( (nteams,nteams), dtype=int)
 start_time = time.time()
@@ -116,11 +117,47 @@ printStandings(s, teams, week_current-1)
 
 # Compute final probabilities as percentages
 prob = (outcomes/nsim)*100
+
+# Playoff odds Is the sum of odds to make any playoff seed
+playoffOdds = []
+seed1 = []
+seed2 = []
+seed3 = []
+seed4 = []
+seed5 = []
+seed6 = []
+for idx, t in enumerate(teams):
+    playoffOdds.append(sum(prob[idx][0:nseeds]))
+    seed1.append(prob[idx][0])
+    seed2.append(prob[idx][1])
+    seed3.append(prob[idx][2])
+    seed4.append(prob[idx][3])
+    seed5.append(prob[idx][4])
+    seed6.append(prob[idx][5])
+
+# Save into pandas dataframe for plotting later
+df = pd.DataFrame({"Team name": [t.team_name for t in teams],
+                   "Percent chance to make playoffs": playoffOdds,
+                   "Percent chance to make 1 seed": seed1,
+                   "Percent chance to make 2 seed": seed2,
+                   "Percent chance to make 3 seed": seed3,
+                   "Percent chance to make 4 seed": seed4,
+                   "Percent chance to make 5 seed": seed5,
+                   "Percent chance to make 6 seed": seed6,
+                   })
+
+# Print to check it
+# print(df)
+
+# Save it for later
+filename = leaguename + "_playoff_odds_heading_into_week" + str(week_current) + "_year" + str(year) + "_nsim" + str(nsim) + ".pkl"
+df.to_pickle(filename)
+
 # Write results to csv file
-filename = "playoff_odds_" + leaguename + "_heading_into_week_" + str(week_current) + "_year_" + str(year) + ".csv"
+filename = leaguename + "_playoff_odds_heading_into_week" + str(week_current) + "_year" + str(year) + "_nsim" + str(nsim) + ".csv"
 with open(filename, mode='w', newline='') as csvfile:
     fwriter = csv.writer(csvfile)
     fwriter.writerow(['Team name','% chance make playoffs','% chance 1 seed','% chance 2 seed','% chance 3 seed','% chance 4 seed','% chance 5 seed','% chance 6 seed'])
     for idx, t in enumerate(teams):
-        lst = [t.team_name, sum(prob[idx][0:nseeds]), prob[idx][0], prob[idx][1], prob[idx][2], prob[idx][3], prob[idx][4], prob[idx][5]]
+        lst = [t.team_name, sum(prob[idx][0:nseeds]), prob[idx][0], prob[idx][1], prob[idx][2], prob[idx][3],prob[idx][4],prob[idx][5]]
         fwriter.writerow(lst)
